c03436a4629558556e08f35b99a0f0440f449341
DavidlunaT <daluna@espol.edu.ec>
2023-11-08 15:40:08 -0500

    proyecto

---
+++ Taller_UML/src/main/java/ec/edu/espol/taller_uml/Taller_UML.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.taller_uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller_UML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
